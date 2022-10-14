559703fa54ef85854b2c88d709993edcc044b081
Saeqa Sultani <saeqasultani@gmail.com>
2022-10-14 14:02:15 +0430

    solved the linters errors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
-import Missions from './components/missions/Missions';
 
 function App() {
   return (
@@ -10,7 +9,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/Missions" element={<Missions />} />
+        
       </Routes>
     </Router>
 
