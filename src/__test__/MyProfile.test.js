bb8bb3441c706def26f05278939e0a7c0a4c3615
Saeqa Sultani <saeqasultani@gmail.com>
2022-10-15 12:42:03 +0430

    solved the linters errors

--- src/__test__/MyProfile.test.js
+++ src/__test__/MyProfile.test.js
@@ -1,12 +1,9 @@
-
 import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import '@testing-library/jest-dom';
-import Rockets from '../components/rockets/Rockets';
 import store from '../redux/configureStore';
 import Profile from '../components/my_profile/MyProfile';
-
 
 it('Check any changes to the component', () => {
   const tree = renderer.create(
